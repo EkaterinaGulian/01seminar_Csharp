186dc94ce7e10bdc99bd1398bcc05b9d6e368459
Ekaterina <katerina3@mail.ru>
2023-02-14 20:08:18 +0300

    task 2 class

---
+++ task02class/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите день недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if(day == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+else if(day == 2)
+{
+  System.Console.WriteLine("Вторник");  
+} 
+else if(day == 3)
+{
+  System.Console.WriteLine("Среда");  
+} 
+else if(day == 4)
+{
+  System.Console.WriteLine("Четверг");  
+} 
+else if(day == 5)
+{
+  System.Console.WriteLine("Пятница");  
+} 
+else if(day == 6)
+{
+  System.Console.WriteLine("Суббота");  
+} 
+else if(day == 7)
+{
+  System.Console.WriteLine("Воскресенье");  
+} 
+else if(day == 8)
+{
+  System.Console.WriteLine("Такого дня недели не существует");  
+} 
+
