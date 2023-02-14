e4e3a772961c98e6aafac17825c6b1ae280666fb
Ekaterina <katerina3@mail.ru>
2023-02-14 20:12:36 +0300

    task 4 class

---
+++ task04class/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N > 99 && N < 1000)
+{
+    System.Console.WriteLine(N%10);
+}
+else
+{
+    System.Console.WriteLine("введено не трехзначное число");
+}
