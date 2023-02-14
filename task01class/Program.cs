77767fe4f3acde8cebac605f6f234627cd83178a
Ekaterina <katerina3@mail.ru>
2023-02-14 20:04:17 +0300

    task 1 class

---
+++ task01class/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("является ли первое число квадратом второго числа?");
+Console.WriteLine("введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите второе целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB * numberB)
+{
+    Console.WriteLine("да");
+}
+else 
+{
+    Console.WriteLine("нет");
+}
