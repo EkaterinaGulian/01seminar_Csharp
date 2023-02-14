4f8edee34b890441f00b60508b8a0d31b89ee1d6
Ekaterina <katerina3@mail.ru>
2023-02-14 20:10:31 +0300

    task 3 class

---
+++ task03class/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = - number; i <=number; i++)
+{
+    System.Console.WriteLine(i);
+}
+// System.Console.Write(i + " "); - в строчку через пробел
